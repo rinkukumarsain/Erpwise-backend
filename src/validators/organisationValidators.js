bf3adfc297c17834c2ceab333e79e6d33cc7ce1e
Devanshu Gautam <85105677+devanshugautam@users.noreply.github.com>
2023-12-01 15:51:22 +0530

    Master data (#6)
    * currency model setup and get api setup
    
    * token middleware setup
    
    * added validators

---
+++ src/validators/organisationValidators.js
@@ -0,0 +1,30 @@
+const { Joi } = require('express-validation');
+
+exports.createOrganisation = {
+    body: Joi.object({
+        companyName: Joi.string().required(),
+        website: Joi.string().required(),
+        email: Joi.string().required(),
+        phone: Joi.string().required(),
+        industryType: Joi.string().required(),
+        currency: Joi.string().required(),
+        documents: Joi.array().optional()
+    })
+};
+
+exports.getAllVat = {
+    query: Joi.object({
+        isActive: Joi.string().enum(['true', 'false']).optional()
+    })
+};
+
+exports.updateOrganisation = {
+    body: Joi.object({
+        companyName: Joi.string().optional(),
+        website: Joi.string().optional(),
+        email: Joi.string().optional(),
+        phone: Joi.string().optional(),
+        industryType: Joi.string().optional(),
+        documents: Joi.array().optional()
+    })
+};
---
+++ src/validators/paymentTermsValidators.js
@@ -0,0 +1,21 @@
+const { Joi } = require('express-validation');
+
+exports.createPaymentTerms = {
+    body: Joi.object({
+        name: Joi.string().required(),
+        noOfDays: Joi.number().required()
+    })
+};
+
+exports.getAllPaymentTerms = {
+    query: Joi.object({
+        isActive: Joi.string().enum(['true', 'false']).optional()
+    })
+};
+
+exports.updatePaymentTerms = {
+    body: Joi.object({
+        name: Joi.string().optional(),
+        noOfDays: Joi.number().optional()
+    })
+};

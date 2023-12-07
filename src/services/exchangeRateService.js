0f329d0b5ce795d945f29a6f571e1c350ce73a7b
rinku sain <35990038+rinkukumarsain@users.noreply.github.com>
2023-12-07 13:13:07 +0530

    Nandlal (#23)
    * faist commit
    
    * change
    
    * exchangeRate create
    
    * edit And get exchangeRate
    
    * change
    
    * Activity in exchangerates

--- src/services/exchangeRateService.js
+++ src/services/exchangeRateService.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 // Local Import
 const { organisationModel, exchangeRateModel } = require('../dbModel');
 const { query } = require('../utils/mongodbQuery');
@@ -10,16 +11,18 @@ const LOG_ID = 'services/exchangeRateService';
 /**
  * Authenticates a user by verifying their credentials.
  *
- * @param {object} reqBody - The request body containing `email` and `password`.
+ * @param {object}  reqBody - The request body containing `email` and `password`.
+ * @param {object}  auth - Data of logedin user.
+ * @param {object}  orgId - The request body containing origanisation information
  * @returns {object} - An object with authentication results:
  *   - `success` (boolean): Indicates whether the authentication was successful.
  *   - `message` (string): A message describing the result of the authentication.
  *   - `data` (Object): User data if authentication is successful.
  *   - `token` (string): JWT token if authentication is successful.
  */
-exports.create = async (reqBody) => {
+exports.create = async (auth, reqBody, orgId) => {
     try {
-        const { orgId, orgCurrency } = reqBody;
+        const { orgCurrency } = reqBody;
         const findOrganisation = await query.findOne(organisationModel, { _id: orgId }, { currency: 1 });
         if (!findOrganisation) {
             return {
@@ -56,6 +59,14 @@ exports.create = async (reqBody) => {
                 message: 'Dates clash, lets reschedule ...!'
             };
         }
+        reqBody.orgId = orgId;
+        const { email, _id } = auth;
+        let obj = {
+            performedBy: _id,
+            performedByEmail: email,
+            actionName: `exchangeRate creation by ${auth.fname} at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`
+        };
+        reqBody.Activity = [obj];
 
         const data = await query.create(exchangeRateModel, reqBody);
         return {
@@ -76,13 +87,15 @@ exports.create = async (reqBody) => {
  * Authenticates a user by verifying their credentials.
  *
  * @param {object} reqBody - The request body containing `email` and `password`.
+ * @param {object} auth - The request body containing `email` and `password`.
+ * @param {object} _id - The request body containing `email` and `password`.
  * @returns {object} - An object with authentication results:
  *   - `success` (boolean): Indicates whether the authentication was successful.
  *   - `message` (string): A message describing the result of the authentication.
  *   - `data` (Object): User data if authentication is successful.
  *   - `token` (string): JWT token if authentication is successful.
  */
-exports.updated = async (reqBody) => {
+exports.updated = async (auth, reqBody, _id) => {
     try {
         let dateCheck;
         const { orgId, orgCurrency } = reqBody;
@@ -105,7 +118,7 @@ exports.updated = async (reqBody) => {
             dateCheck = await query.findOne(exchangeRateModel, {
                 $expr: {
                     $and: [
-                        { $ne: [new mongoose.Types.ObjectId(reqBody._id), '$_id'] },
+                        { $ne: [new mongoose.Types.ObjectId(_id), '$_id'] },
                         { $lte: [new Date(reqBody.startDate), '$endDate'] },
                         { $gte: [new Date(reqBody.startDate), '$startDate'] }
                     ]
@@ -117,7 +130,7 @@ exports.updated = async (reqBody) => {
             dateCheck = await query.findOne(exchangeRateModel, {
                 $expr: {
                     $and: [
-                        { $ne: [new mongoose.Types.ObjectId(reqBody._id), '$_id'] },
+                        { $ne: [new mongoose.Types.ObjectId(_id), '$_id'] },
                         { $lte: [new Date(reqBody.endDate), '$endDate'] },
                         { $gte: [new Date(reqBody.endDate), '$startDate'] }
                     ]
@@ -130,8 +143,13 @@ exports.updated = async (reqBody) => {
                 message: 'Dates clash, lets reschedule ...!'
             };
         }
-
-        const data = await exchangeRateModel.findByIdAndUpdate({ _id: reqBody._id }, reqBody, { new: true });
+        let obj = {
+            performedBy: auth._id,
+            performedByEmail: auth.email,
+            actionName: `exchangeRate updated by ${auth.fname} at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`
+        };
+        reqBody['$push'] = { Activity: obj };
+        const data = await exchangeRateModel.findByIdAndUpdate({ _id: _id }, reqBody, { new: true });
         return {
             success: true,
             message: 'You have successfully updated exchange rate',
@@ -149,14 +167,16 @@ exports.updated = async (reqBody) => {
 /**
  * Authenticates a user by verifying their credentials.
  *
- * @param {object} reqQuery - The request query containing 'orgId', 'isActive' .
+ * @param {object} reqQuery - The request query containing  'isActive' .
+ * @param {object} orgId - The request query containing originasation id 
  * @returns {Array} - An object with authentication results:
  *   - `success` (boolean): Indicates whether the authentication was successful.
  *   - `message` (string): A message describing the result of the authentication.
  *   - `data` (Array): exchangeRate data if authentication is successful.
  */
-exports.getExchangeRate = async (reqQuery) => {
+exports.getExchangeRate = async (reqQuery, orgId) => {
     try {
+        reqQuery.orgId = orgId;
         const pipeLine = getExchangeRatePipeline(reqQuery);
         const data = await query.aggregation(exchangeRateModel, pipeLine);
         return {

51d982bdeb4d388a0a9e74d4b1fd41c92cd4b41f
Devanshu Gautam <85105677+devanshugautam@users.noreply.github.com>
2023-12-14 20:38:04 +0530

    Master data (#68)
    * currency model setup and get api setup
    
    * token middleware setup
    
    * added validators
    
    * 05/12/2023 | added multer s3 and created edit user and user status api
    
    * lead creation
    
    * change
    
    * change
    
    * change
    
    * change
    
    * change
    
    * edied aggregation
    
    * edied aggregation
    
    * change
    
    * leads get and add api
    
    * change:
    
    * change:
    
    * change
    
    * cahgng
    
    * cahgne
    
    * change
    
    * lead contact
    
    * change
    
    * change
    
    * change
    
    * change
    
    * qualify lead api , lead level, crm level logic done.
    
    * add prospect api and edit prospect qulifyMeta detials api created.
    
    * edited get all lead api logic
    
    * edited get all lead api added pagination and sort
    
    * get all lead api edit
    
    * get all lead api edit
    
    * change
    
    * lead  finance add api.
    
    * code
    
    * change
    
    * change
    
    * cjangfe
    
    * change
    
    * chagne
    
    * enquiry model created
    
    * lead service edit
    
    * get pipeline data api
    
    * eslint file lf/crlf file type error issue resolved
    
    * change
    
    * change
    
    * change
    
    * change
    
    * change
    
    * change
    
    * cahge
    
    * change
    
    * change
    
    * change
    
    * change
    
    * supplier contacts
    
    * change
    
    * cahnge
    
    * change
    
    * supplier address apis
    
    * commit
    
    * change
    
    * change

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,5 +20,5 @@ module.exports = (app) => {
     app.use(`${apiV1Prefix}/leadAddress`, require('./leadAddress'));
     app.use(`${apiV1Prefix}/supplier`, require('./supplier'));
     app.use(`${apiV1Prefix}/supplierContact`, require('./supplierContact'));
-    app.use(`${apiV1Prefix}/supplierContact`, require('./supplierAddress'));
+    app.use(`${apiV1Prefix}/supplierAddress`, require('./supplierAddress'));
 };

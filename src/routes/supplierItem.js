e50fc2ecfcff6593e0778916c44a3c75937f4552
devanshuimg <devanshugautam0@gmail.com>
2023-12-27 00:22:45 +0530

    created supplier item bulk upload api

--- src/routes/supplierItem.js
+++ src/routes/supplierItem.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 const { validate } = require('express-validation');
 
 // Local imports
 const { logger } = require('../utils/logger');
+const { upload } = require('../utils/multer');
 const { statusCode } = require('../../config/default.json');
 const { handleResponse, handleErrorResponse } = require('../helpers/response');
 const { supplierItemService } = require('../services');
@@ -74,6 +76,32 @@ router.get('/get/hscode', jwtVerify, async (req, res) => {
         return handleResponse(res, statusCode.BAD_REQUEST, result);
     } catch (err) {
         logger.error(LOG_ID, `Error occurred during supplierItem/get/hscode : ${err.message}`);
+        handleErrorResponse(res, err.status, err.message, err);
+    }
+});
+
+/**
+ * Route for uploading supplier items in bulk.
+ */
+router.post('/bulkupload/:id', jwtVerify, upload.single('file'), async (req, res) => {
+    try {
+        logger.info(LOG_ID, `File to read all item's | File Path :- ${req.file.path}`);
+        console.log('req.file', req.file);
+        const result = await supplierItemService.itemBulkUpload(req.auth, req.headers['x-org-type'], req.params.id, req.file.path);
+        fs.unlink(req.file.path, (err) => {
+            if (err) {
+                console.log(err);
+                logger.error(LOG_ID, `Error occurred while remove file from path (${req.file.path}) : ${err.message}`);
+                return;
+            }
+            logger.info(LOG_ID, `File removed successfully | File Path :- ${req.file.path}`);
+        });
+        if (result.success) {
+            return handleResponse(res, statusCode.OK, result);
+        }
+        return handleResponse(res, statusCode.BAD_REQUEST, result);
+    } catch (err) {
+        logger.error(LOG_ID, `Error occurred during supplierItem/bulkupload/:id : ${err.message}`);
         handleErrorResponse(res, err.status, err.message, err);
     }
 });
